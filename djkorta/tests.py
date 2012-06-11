95bda175b7bd7a678d3fb561dd4d63384a94f0ad
Stefan Kjartansson <esteban.supreme@gmail.com>
2012-06-11 22:35:36 +0000

    Added tests for charging references

--- djkorta/tests.py
+++ djkorta/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 
+from . import CreditCard
 from .forms import PaymentInfoForm
+from .models import Order, OrderException, Customer
 
 
 class PaymentInfoFormTest(TestCase):
@@ -17,7 +19,12 @@ class PaymentInfoFormTest(TestCase):
 
         #charge $1
         order = f.process(100, currency='USD')
-        self.assertEqual(order.state, 'SUCCESS')
+        self.assertEqual(order.status, 'SUCCESS')
+        self.assertEqual(Order.objects.successful().count(), 1)
+        self.assertEqual(Order.objects.by_currency_code('USD') \
+            .successful().count(), 1)
+        with self.assertRaises(OrderException):
+            order.amount = 200
 
     def test_invalid_creditcard(self):
         post_data = {
@@ -36,8 +43,9 @@ class PaymentInfoFormTest(TestCase):
         }
         self.assertEqual(PaymentInfoForm(post_data).is_valid(), False)
 
-'''
-    def test_helpers(self):
-        f = PaymentInfoForm()
-        self.assertIsNotNone(f.helper)
-'''
+    def test_customer_order(self):
+        c = Customer(credit_card=CreditCard('4571999400007492', 5, 14, 123))
+        c.save()
+        self.assertTrue(c.charge(2000))
+        self.assertEqual(c.orders.count(), 1)
+        c.delete()
