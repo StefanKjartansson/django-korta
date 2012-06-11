8955c9f089986bb1dcc5698c0e3ae5c73d4dc3aa
Stefan Kjartansson <esteban.supreme@gmail.com>
2012-06-11 22:26:47 +0000

    get_default_client is not a function, added license

--- djkorta/__init__.py
+++ djkorta/__init__.py
@@ -5,25 +5,31 @@
 djkorta
 ~~~~~~~
 
+:copyright: (c) 2012 by the Stefán Kjartansson, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
 """
 from __future__ import absolute_import
 
 from .conf import settings
 
-from korta import Client, korta_reference
+from korta import Client, korta_reference, CreditCard
 from korta.defaults import CURRENCY_CODES
 
 
-if hasattr(settings, 'KORTA_URL'):
-    client = Client.init_from_url(settings.KORTA_URL)
-else:
-    client = Client(
-        settings.KORTA_USER,
-        settings.KORTA_USER_PASSWORD,
-        settings.KORTA_HOST,
-        settings.KORTA_PORT,
-        settings.KORTA_SITE_ID,
-        settings.KORTA_CARD_ACCEPTOR_ID,
-        settings.KORTA_CARD_ACCEPTOR_IDENTITY,
-        settings.KORTA_PEM,
-        settings.KORTA_DEFAULT_CURRENCY)
+def get_default_client():
+    if hasattr(settings, 'KORTA_URL'):
+        return Client.init_from_url(settings.KORTA_URL)
+    else:
+        return Client(
+            settings.KORTA_USER,
+            settings.KORTA_USER_PASSWORD,
+            settings.KORTA_HOST,
+            settings.KORTA_PORT,
+            settings.KORTA_SITE_ID,
+            settings.KORTA_CARD_ACCEPTOR_ID,
+            settings.KORTA_CARD_ACCEPTOR_IDENTITY,
+            settings.KORTA_PEM,
+            settings.KORTA_DEFAULT_CURRENCY)
+
+
+client = get_default_client()
