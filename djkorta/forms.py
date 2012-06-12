fb3879e407a6a6776625a00e2459a7cef946673b
Stefan Kjartansson <esteban.supreme@gmail.com>
2012-06-12 14:58:50 +0000

    Reusing clean function elsewhere

--- djkorta/forms.py
+++ djkorta/forms.py
@@ -13,10 +13,10 @@ import datetime
 from collections import namedtuple
 
 from django import forms
-from django.conf import settings
 from django.utils.translation import ugettext as _
 
 from . import client
+from .conf import settings
 from .fields import CreditCardField
 from .models import Order
 
@@ -26,9 +26,31 @@ MONTHS = [(i, i) for i in [str(i)
     if len(str(i)) > 1 else ('0%d' % i)
     for i in range(1, 13)]]
 
-CC_YEARS = getattr(settings, 'CREDIT_CARD_YEARS', 15)
-
 CreditCard = namedtuple('CreditCard', ['number', 'expires', 'ccv'])
+
+
+def cc_clean(self):
+    data = self.cleaned_data
+    if not data.get('number', None):
+        raise forms.ValidationError(
+            _(u'Inavlid number'))
+    exp_year = data.get('expiration_year', None)
+    if not exp_year:
+        raise forms.ValidationError(
+            _(u'Expiration date is invalid'))
+    y = str(exp_year)
+    m = data['expiration_month']
+    if len(y) == 2:
+        y = '20' + y
+    expiration_date = datetime.date(int(y), int(m), 1)
+    if expiration_date < datetime.date.today():
+        raise forms.ValidationError(
+            _(u'Expiration date is invalid'))
+    data['expiration_date'] = expiration_date
+    data['credit_card'] = CreditCard(number=str(data['number']),
+        expires=data['expiration_date'].strftime('%y%m'),
+        ccv=data['ccv'])
+    return data
 
 
 class PaymentInfoForm(forms.Form):
@@ -39,7 +61,7 @@ class PaymentInfoForm(forms.Form):
         self.fields['expiration_year'].choices = [
             (i[-2:], i) for i in [
                 str(datetime.datetime.now().year + i)
-                for i in range(0, CC_YEARS)]]
+                for i in range(0, settings.KORTA_CREDIT_CARD_YEARS)]]
 
     number = CreditCardField(required=True,
         label=_(u"Card Number"))
@@ -52,30 +74,13 @@ class PaymentInfoForm(forms.Form):
         max_value=9999,
         widget=forms.TextInput(attrs={'size': '4'}))
 
-    def clean(self):
-        data = self.cleaned_data
-        exp_year = data.get('expiration_year', None)
-        if not exp_year:
-            raise forms.ValidationError(
-                _(u'Expiration date is invalid'))
-        y = str(exp_year)
-        m = data['expiration_month']
-        if len(y) == 2:
-            y = '20' + y
-        expiration_date = datetime.date(int(y), int(m), 1)
-        if expiration_date < datetime.date.today():
-            raise forms.ValidationError(
-                _(u'Expiration date is invalid'))
-        data['expiration_date'] = expiration_date
-        return data
+    clean = cc_clean
 
     def process_order(self, order):
         data = self.clean()
-        order.status = ('SUCCESS' if client.one_off(order,
-            CreditCard(number=str(data['number']),
-                expires=data['expiration_date'].strftime('%y%m'),
-                ccv=data['ccv']))
-            else 'ERROR')
+        order.status = (Order.STATUS.SUCCESS
+            if client.one_off(order, data['credit_card'])
+                else Order.STATUS.ERROR)
         order.save()
         return order
 
